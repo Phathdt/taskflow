7c204de8b161a9caa80737e81ac9794236093306
Phathdt <phathdt379@gmail.com>
2026-02-26 08:40:43 +0700

    fix: update healthcheck response to return 'ok1'

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common'
 export class AppController {
   @Get('/healthcheck')
   healthcheck() {
-    return { data: 'ok' }
+    return { data: 'ok1' }
   }
 }
